b8049554da93e4a772e79227b15c2d72677b92f8
Ismael Rumzan <ismael.rumzan@gmail.com>
2021-08-22 19:26:51 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+		{
+			"source": "/(.*)",
+			"has": [{
+				"type": "host",
+				"value": "(?<sub>.*)\\.nextjs-rewrite-subdomain2path.vercel-support\\.app"
+			}],
+			"destination": "/user/:sub"
+		}
+    ]
+  },
 }
